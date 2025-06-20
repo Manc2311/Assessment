3d6c523ce37ef43e940256d0d2e2dd7ec0a4843b
Manasi <manasi.chavan@softlabsgroup.com>
2025-06-20 12:07:36 +0530

    changes for deploymnet

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,6 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 })
